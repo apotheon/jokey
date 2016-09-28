387290c2b5717f6ec877dc82b599f7a84b979804
Chad Perrin <chad@radialdevgroup.com>
2016-09-28 16:43:17 -0600

    LICENSE, README, index.php stub

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Jokey Title</title>
+  </head>
+
+  <body>
+    <h1>Jokey Heading</h1>
+    <?php $db = mysqli_connect('localhost', 'root', 'root', 'scotchbox'); ?>
+    <?php phpinfo(); ?>
+  </body>
+</html>
