551703d29213b9c9431415a14d7412dc892760c4
Chad Perrin <chad@radialdevgroup.com>
2016-09-28 17:55:36 -0600

    show data

--- index.php
+++ index.php
@@ -12,5 +12,16 @@
       <br />
       <input type="submit" name="save" value="save" />
     </form>
+
+    <?php $result = mysqli_query($db, "select * from notes"); ?>
+
+    <table>
+      <?php while ($row = mysqli_fetch_array($result)) { ?>
+      <tr>
+        <td><?php echo $row['created'] ?></td>
+        <td style="border: thin solid black;"><?php echo $row['note'] ?></td>
+      </tr>
+      <?php } ?>
+    </table>
   </body>
 </html>
