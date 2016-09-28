13a0170a7ee57f5d9fc72e90ecd817afa1f194a0
Chad Perrin <chad@radialdevgroup.com>
2016-09-28 17:45:44 -0600

    try to redirect correctly

--- add_note.php
+++ add_note.php
@@ -1,30 +1,21 @@
 <?php
 
-$note = $_POST["note"];
+$note = $_POST['note'];
 
-$host="localhost"; // Host name
-$username="root"; // Mysql username
-$password="root"; // Mysql password
-$db_name="scotchbox"; // Database name
-$tbl_name="notes"; // Table name
-
-// Connect to server and select database.
 $db = mysqli_connect('localhost', 'root', 'root', 'scotchbox') or die(
-  "connection failed"
+  'connection failed'
 );
 
-// mysql_connect("$host", "$username", "$password") or die("connection failed");
-
-// mysql_select_db("$db_name") or die("cannot select DB");
-
-$result = mysqli_query($db, "INSERT INTO notes ( note ) VALUES ( '$note' )");
+$result = mysqli_query(
+  $db, "INSERT INTO notes ( note ) VALUES ( '$note' )"
+);
 
 if ($result) {
   mysqli_close($db);
-  header("/");
+  header('Location: ' . '/index.php');
 } else {
   mysqli_close($db);
-  echo "ERROR";
+  echo 'ERROR';
 }
 
 ?>
